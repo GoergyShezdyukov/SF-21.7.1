4687dc5cffb99c1bb19b33c3dc30b9ba6bae00dd
Шездюков Георгий <georgy.shezdyukov@yandex.ru>
2023-07-30 14:18:36 +0300

    Add files via upload

---
+++ Pets.py
@@ -0,0 +1,9 @@
+from DesignerPets import Cat
+
+p1 = Cat('Барон ', 'Мальчик', 2)
+p2 = Cat('Сэм ', 'Мальчик', 2)
+
+print('Имя    / пол     / возраст')
+print(p1.name, p1.gender, p1.age)
+print('-' * 15)
+print(p2.name, p2.gender, p2.age)
